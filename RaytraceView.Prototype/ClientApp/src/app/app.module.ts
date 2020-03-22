2e562998e7b54e7a8b3861d39ea072ae09b3affa
Rabbid76 <Gernot.Steinegger@gmail.com>
2020-03-22 08:32:17 +0100

    clean code, remove unnecessary components

--- RaytraceView.Prototype/ClientApp/src/app/app.module.ts
+++ RaytraceView.Prototype/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-//import { CounterComponent } from './counter/counter.component';
-//import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { AboutComponent } from './about/about.component';
 import { Rt1InOneWeekendComponent } from './rt1_in_one_weekend/rt1_in_one_weekend.component';
 import { HealthCheckComponent } from './health-check/health-check.component';
@@ -18,8 +16,6 @@ import { HealthCheckComponent } from './health-check/health-check.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    //CounterComponent,
-    //FetchDataComponent,
     Rt1InOneWeekendComponent,
     AboutComponent,
     HealthCheckComponent
@@ -30,8 +26,6 @@ import { HealthCheckComponent } from './health-check/health-check.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      //{ path: 'counter', component: CounterComponent },
-      //{ path: 'fetch-data', component: FetchDataComponent },
       { path: 'rt1_in_one_weekend', component: Rt1InOneWeekendComponent },
       { path: 'about', component: AboutComponent },
       { path: 'health-check', component: HealthCheckComponent }
