4db5311ffad5726c114c06f617f34b9ce6851a47
Rabbid76 <Gernot.Steinegger@gmail.com>
2020-04-12 20:50:28 +0200

    ray trace view prototype: client side validation

--- RaytraceView.Prototype/ClientApp/src/app/raytrace/raytracecontrols/raytracecontrols.component.ts
+++ RaytraceView.Prototype/ClientApp/src/app/raytrace/raytracecontrols/raytracecontrols.component.ts
@@ -1,8 +1,16 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RayTraceService } from '../../services/raytrace.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 import { RayTraceParameter } from '../../services/model/raytracemodels';
+
+function psitiveNonZeroNumber(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const forbidden = Number(control.value) <= 0;
+    return forbidden ? { 'lessOrEqualZero': { value: control.value } } : null;
+  };
+}
+
 
 @Component({
   selector: 'app-raytrace-controls',
@@ -23,12 +31,20 @@ export class RayTraceControlsComponent {
     this.scenes = this.service.raytrace.scenes;
     this.form = new FormGroup({
       sceneName: new FormControl(this.service.raytrace.parameter.sceneName, Validators.required),
-      width: new FormControl(this.service.raytrace.parameter.width, Validators.required),
-      height: new FormControl(this.service.raytrace.parameter.height, Validators.required),
-      samples: new FormControl(this.service.raytrace.parameter.samples, Validators.required),
+      width: new FormControl(this.service.raytrace.parameter.width, [Validators.required, psitiveNonZeroNumber()]),
+      height: new FormControl(this.service.raytrace.parameter.height, [Validators.required, psitiveNonZeroNumber()]),
+      samples: new FormControl(this.service.raytrace.parameter.samples, [Validators.required, psitiveNonZeroNumber()]),
       updaterate: new FormControl(this.service.raytrace.parameter.updateRate * 100, Validators.required),
     }, null, null);
     this.service.controls = this;
+  }
+
+  static positiveNumber(control: FormControl): { [key: string]: any; } {
+    if (Number(control.value) > 0) {
+      return { positiveNumber: true };
+    } else {
+      return null;
+    }
   }
 
   onSubmit() {
